325a3005c99cd9cc269fcc788516320c31c97008
James Bender <James@JamesCBender.com>
2015-01-19 11:38:18 -0500

    got artist contoller done through unit testing

---
+++ API/AngularMusicStore/AngularMusicStore.Api/Controllers/ArtistController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AngularMusicStore.Api.Models;
+using AngularMusicStore.Api.Models.ViewModels;
+
+namespace AngularMusicStore.Api.Controllers
+{
+    public class ArtistController : ApiController
+    {
+        private readonly IArtistModel _artistModel;
+
+        public ArtistController(IArtistModel artistModel)
+        {
+            _artistModel = artistModel;
+        }
+
+        public IEnumerable<Artist> GetArtists()
+        {
+            return _artistModel.GetArtists();
+        }
+
+        [ResponseType(typeof(Artist))]
+        public HttpResponseMessage GetById( string artistId)
+        {
+            Guid retrievalId;
+            if (!Guid.TryParse(artistId, out retrievalId))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var artist = _artistModel.GetById(Guid.Parse(artistId));
+            return artist == null
+                ? Request.CreateResponse(HttpStatusCode.NotFound)
+                : Request.CreateResponse(HttpStatusCode.OK, artist);
+        }
+
+        public Guid PostArtist(Artist artist)
+        {
+            return _artistModel.Save(artist);
+        }
+
+        public HttpResponseMessage PutArtist(Artist artist)
+        {
+            var returnStatus = HttpStatusCode.OK;
+
+            if (artist.Id == Guid.Empty)
+            {
+                returnStatus = HttpStatusCode.Created;
+            }
+
+            _artistModel.Save(artist);
+            
+            return Request.CreateResponse(returnStatus);
+        }
+
+        public HttpResponseMessage Delete(Artist artist)
+        {
+            if (_artistModel.GetById(artist.Id) == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            _artistModel.Delete(artist);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ API/AngularMusicStore/AngularMusicStore.UnitTests/Web/Controller/ArtistControllerTests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AngularMusicStore.Api.Controllers;
+using AngularMusicStore.Api.Models;
+using AngularMusicStore.Api.Models.ViewModels;
+using Moq;
+using NUnit.Framework;
+
+namespace AngularMusicStore.UnitTests.Web.Controller
+{
+    [TestFixture]
+    public class ArtistControllerTests
+    {
+        private ArtistController _artistController;
+        private Mock<IArtistModel> _artistModel;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            _artistModel = new Mock<IArtistModel>();
+            _artistController = new ArtistController(_artistModel.Object)
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+        }
+
+        [Test]
+        public void ShouldBeAbleToGetAListOfAllArtists()
+        {
+            var listOfArtists = new List<Artist> {new Artist(), new Artist(), new Artist()};
+            var numberOfArtists = listOfArtists.Count;
+            _artistModel.Setup(x => x.GetArtists()).Returns(listOfArtists);
+
+            var result = _artistController.GetArtists();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(numberOfArtists, result.Count());
+            _artistModel.Verify(x => x.GetArtists(), Times.Once);
+        }
+
+        [Test]
+        public void ShouldBeAbleToGetASpecificArtistByIdWhenIdIsValidGuidAndArtistExists()
+        {
+            var artistId = Guid.NewGuid();
+            var artist = new Artist {Id = artistId};
+            _artistModel.Setup(x => x.GetById(artistId)).Returns(artist);
+
+            var result = _artistController.GetById(artistId.ToString());
+            
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.TryGetContentValue(out artist));
+            Assert.IsNotNull(artist);
+            Assert.AreEqual(artistId, artist.Id);
+        }
+
+        [Test]
+        public void ShouldGetBackAnHttp400ErrorForMalformedArtistId()
+        {
+            var result = _artistController.GetById("this is not a guid");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+            _artistModel.Verify(x => x.GetById(It.IsAny<Guid>()), Times.Never);
+        }
+
+        [Test]
+        public void ShouldGetBackAnHttp404ErrorForNoAristFound()
+        {
+            var result = _artistController.GetById(Guid.NewGuid().ToString());
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(HttpStatusCode.NotFound, result.StatusCode);
+        }
+
+        [Test]
+        public void ShouldBeAbleToPostANewArtist()
+        {
+            var artist = new Artist();
+
+            var result = _artistController.PostArtist(artist);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void ShouldBeAbleToPutAnNewArtist()
+        {
+            var artist = new Artist();
+
+            var result = _artistController.PutArtist(artist);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(HttpStatusCode.Created, result.StatusCode);
+        }
+
+        [Test]
+        public void ShouldBeAbleToPutAnExistingArtist()
+        {
+            var artistId = Guid.NewGuid();
+            var artist = new Artist {Id = artistId};
+
+            var result = _artistController.PutArtist(artist);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Test]
+        public void ShouldBeAbleToDeleteAnExistingArtist()
+        {
+            var artistId = Guid.NewGuid();
+            var artist = new Artist {Id = artistId};
+            _artistModel.Setup(x => x.GetById(artistId)).Returns(artist);
+
+            var result = _artistController.Delete(artist);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Test]
+        public void ShouldGetBackAnHttp404StatusWhenArtistToBeDeleteDoesNotExist()
+        {
+            var result = _artistController.Delete(new Artist());
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(HttpStatusCode.NotFound, result.StatusCode);
+        }
+    }
+}
