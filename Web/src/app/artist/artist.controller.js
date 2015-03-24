44c30c1592f55d1aafcc90d999348bc5e3e3ed5c
James Bender <James@JamesCBender.com>
2015-03-24 10:47:15 -0400

    cleanup

--- Web/src/app/artist/artist.controller.js
+++ Web/src/app/artist/artist.controller.js
@@ -2,12 +2,10 @@
 
 angular.module('musicStore').controller('ArtistCtrl', ['$scope', 'musicStoreAPI', function ($scope, musicStoreAPI) {
 
-    $scope.searchForArtist = function(){
-      var artist = $scope.artistToSearchFor;
-      var results = musicStoreAPI.query({name: artist}, function(data){
-        $scope.result = data;
-        $scope.artistsFound = data;
-      });
-    };
-
-  }]);
+  $scope.searchForArtist = function(){
+    var artist = $scope.artistToSearchFor;
+    var results = musicStoreAPI.query({name: artist}, function(data){
+      $scope.artistsFound = data;
+    });
+  };
+}]);
