08ab51e6ee8f9e6c9077842a137e76fa6e447c13
James Bender <James@JamesCBender.com>
2015-03-26 18:24:26 -0400

    added picture

---
+++ API/AngularMusicStore/AngularMusicStore.UnitTests/Web/Model/AutomapperConfigurationTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using AngularMusicStore.Api.Models.ViewModels;
+using AutoMapper;
+using NUnit.Framework;
+using Domain = AngularMusicStore.Core.Entities;
+
+namespace AngularMusicStore.UnitTests.Web.Model
+{
+    [TestFixture]
+    public class AutomapperConfigurationTests
+    {
+        [Test]
+        public void ArtistImageUrlShouldHaveBaseUrlPutOnFront()
+        {
+            AutomapperConfiguration.Configure();
+
+            const string imageName = "imageName";
+            var basePath = ConfigurationManager.AppSettings["ImageBasePath"];
+
+            var domainArtist = new Domain.Artist {PictureUrl = imageName};
+            
+            var result = Mapper.Map<Domain.Artist, Artist>(domainArtist);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(basePath + imageName, result.PictureUrl);
+        }
+    }
+}
