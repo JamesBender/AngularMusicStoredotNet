2b5c726d09e22c1868308021672cd8756f423662
James Bender <James@JamesCBender.com>
2015-08-05 10:47:58 -0400

    added a threecolumn intial view to the artists view

--- Web/src/app/artist/artist.controller.js
+++ Web/src/app/artist/artist.controller.js
@@ -3,7 +3,7 @@
 angular.module('musicStore').controller('ArtistCtrl', ['$scope', 'musicStoreArtistAPI', function ($scope, musicStoreArtistAPI) {
 
 	$scope.artistsFound = [];
-
+	
 	$scope.searchForArtist = function(){
 		var artist = $scope.artistToSearchFor;
 		var results = musicStoreArtistAPI.query({name: artist}, function(data){
@@ -13,5 +13,14 @@ angular.module('musicStore').controller('ArtistCtrl', ['$scope', 'musicStoreArti
 
 	musicStoreArtistAPI.query(function(data){
 		$scope.artistsFound = data;
+		$scope.threeColumnArtistsList = chunk($scope.artistsFound, 3);
 	});
+
+	function chunk(arr, size) {
+		var newArr = [];
+		for (var i=0; i<arr.length; i+=size) {
+			newArr.push(arr.slice(i, i+size));
+		}
+		return newArr;
+	}
 }]);
