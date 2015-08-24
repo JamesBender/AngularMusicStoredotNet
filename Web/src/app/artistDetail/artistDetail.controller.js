8be40d145319abc86dbfe635c43d2d259c84ff32
James Bender <James@JamesCBender.com>
2015-08-23 22:43:55 -0400

    got modal working

--- Web/src/app/artistDetail/artistDetail.controller.js
+++ Web/src/app/artistDetail/artistDetail.controller.js
@@ -4,8 +4,9 @@ angular.module('musicStore').controller('ArtistDetailCtrl', [
 	'$scope', 
 	'$routeParams', 
 	'$location',
+	'$modal',
 	'musicStoreArtistAPI', 
-	function ($scope, $routeParams, $location, musicStoreArtistAPI) {
+	function ($scope, $routeParams, $location, $modal, musicStoreArtistAPI) {
 
 	musicStoreArtistAPI.get({id: $routeParams.id}, function(data){
 		$scope.id = data.Id;
@@ -17,5 +18,34 @@ angular.module('musicStore').controller('ArtistDetailCtrl', [
 
 	$scope.editArtist = function(){
 		$location.path('/artist/edit/' + $scope.id);
+	};
+
+	$scope.deleteArtist = function(){
+		var modalInstance = $modal.open({
+			animation: true,
+			templateUrl: 'confirmArtistDelete.html',
+			controller: 'ArtistDeleteCtrl'
+		});
+
+		modalInstance.result.then(function(outcome){
+			$scope.status = outcome;
+		});
+	};
+
+
+
+	$scope.itWorked = function(){
+		$scope.status = 'modal worked';
+	};
+}]);
+
+angular.module('musicStore').controller('ArtistDeleteCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance){
+
+	$scope.ok = function(){
+		$modalInstance.close(true);
+	};
+
+	$scope.cancel = function(){
+		$modalInstance.close(false);
 	};
 }]);
